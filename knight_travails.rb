f2f68faa21b08d376e20b10e9146e74028b37fe9
Schifferking <schifferking@hotmail.com>
2022-08-29 12:43:55 -0500

    Finished knight_travails function
    This function receives two lists, the start square and the end square.
    The objective is to count the number of moves a knight piece will do to
    move from start to finish and return the path.
    
    To do this, we have three classes, one for the board that will store in
    an array the squares, that are objects from the Vertex class, that has
    coords (x,y), a distance to measure the number of moves and parent, that
    helps to get the path at the end of the function.
    
    This function uses the BFS algorithm to determine the number of moves.
    It creates the graph during the algorithm, creating new squares
    (Vertex), calculating the next moves and checking if these moves are
    valid. It finish when the current knight's position (a vertex) is the
    desired square. Then it uses an auxiliary function to return the path
    using a loop and adding each element to an array.

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,13 +1,11 @@
 class Vertex
-  attr_accessor :value, :edges
+  attr_accessor :x, :y, :dist, :parent
 
-  def initialize(value)
-    @value = value
-    @edges = []
-  end
-
-  def add_edge(vertex)
-    @edges << vertex
+  def initialize(x, y, dist = 0, parent = nil)
+    @x = x
+    @y = y
+    @dist = dist
+    @parent = parent
   end
 end
 
@@ -19,6 +17,63 @@ class Board
   end
 
   def add_square(square)
-    @squares << Vertex.new(square)
+    squares.push square
   end
 end
+
+class Knight
+  attr_accessor :position
+
+  def initialize(position = nil)
+    @position = position
+  end
+end
+
+def valid?(x, y)
+  !(x.negative? || y.negative? || x > 7 || y > 7)
+end
+
+def knights_travails(start_s, end_s)
+  row = [2, 2, -2, -2, 1, 1, -1, -1]
+  col = [-1, 1, 1, -1, 2, -2, 2, -2]
+  board = Board.new
+  knight = Knight.new
+  explored = []
+  queue = []
+  start_square = Vertex.new(start_s.first, start_s.last)
+  board.add_square start_square
+  queue.push start_square
+
+  until queue.empty?
+    knight.position = queue.shift
+    if knight.position.x == end_s.first && knight.position.y == end_s.last
+      puts "You made it in #{knight.position.dist} moves! Here's your path:"
+      return path knight.position
+    end
+
+    unless explored.include? knight.position
+      explored.push knight.position
+      row.length.times do |i|
+        x1 = knight.position.x + row[i]
+        y1 = knight.position.y + col[i]
+
+        if valid?(x1, y1)
+          square_ = Vertex.new(x1, y1, knight.position.dist + 1, knight.position)
+          queue.push square_
+        end
+      end
+    end
+  end
+end
+
+def path(vertex)
+  path = []
+  until vertex.parent.nil?
+    path.push [vertex.x, vertex.y]
+    vertex = vertex.parent
+  end
+  path.push [vertex.x, vertex.y]
+  path.reverse
+end
+
+p knights_travails([3, 3], [4, 3])
